66729b1c8dd643045db06b546a3164523e8b059d
leobartowski <francescodangelo95@gmail.com>
2021-10-21 15:06:22 +0200

    Initial Commit

---
+++ Bdc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bdc
+//
+//  Created by Francesco D'Angelo on 21/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BdcUITests/BdcUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BdcUITestsLaunchTests.swift
+//  BdcUITests
+//
+//  Created by Francesco D'Angelo on 21/10/21.
+//
+
+import XCTest
+
+class BdcUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
