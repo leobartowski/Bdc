2633a783d1c7c2b2fabf418f667102c0a6b88201
leobartowski <francescodangelo95@gmail.com>
2021-12-22 15:54:20 +0100

    WIP

---
+++ Bdc/Model/RankingType.swift
@@ -0,0 +1,15 @@
+//
+//  RankingType.swift
+//  Bdc
+//
+//  Created by leobartowski on 22/12/21.
+//
+
+import Foundation
+
+enum RankingType: Int {
+    
+    case weekly = 0
+    case monthly = 1
+    case yearly = 2
+}
