c3c52898d66c6b6f5ea4bd216c3b7747643e6ea0
leobartowski <francescodangelo95@gmail.com>
2021-12-30 11:43:02 +0100

    FIX: Closed contexDeleteItem automatically

--- Bdc/Controller/Settings/SettingsViewController.swift
+++ Bdc/Controller/Settings/SettingsViewController.swift
@@ -15,7 +15,6 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     
     override func viewDidLoad() {
         self.setupTableViewShadow()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
