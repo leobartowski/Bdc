9f90cb2384e273965628a0b911dab79c18fbc6d4
leobartowski <francescodangelo95@gmail.com>
2022-01-09 18:07:41 +0100

    DONE:
    - Added different cell in RankingVC improve performance
    - Added diagonalStrikeThrough to holiday days in collectionView of weekdays RankingVC
    - improved spacing CollectionView collectionView of weekdays RankingVC

--- Bdc/Cells/Ranking/RankingVC/RankingTableViewCell/RankingTableViewCell.swift
+++ Bdc/Cells/Ranking/RankingVC/RankingTableViewCell/RankingTableViewCell.swift
@@ -2,10 +2,9 @@
 //  RankingTableViewCell.swift
 //  Bdc
 //
-//  Created by Francesco D'Angelo on 23/11/21.
+//  Created by leobartowski on 09/01/22.
 //
 
-import Foundation
 import UIKit
 
 class RankingTableViewCell: UITableViewCell {
@@ -15,17 +14,8 @@ class RankingTableViewCell: UITableViewCell {
     @IBOutlet var mainImageView: UIImageView!
     @IBOutlet var attendanceLabel: UILabel!
     @IBOutlet var admonishmentLabel: UILabel!
-    @IBOutlet var collectionView: UICollectionView!
 
     var indexPath = IndexPath()
-
-    let days = ["L", "M", "M", "G", "V"]
-    var morningDaysNumbers: [Int] = []
-    var eveningDaysNumbers: [Int] = []
-
-    var isDetailViewHidden: Bool {
-        return self.collectionView.isHidden
-    }
 
     override func layoutSubviews() {
         self.containerView.layer.shadowPath = UIBezierPath(roundedRect: self.containerView.bounds, cornerRadius: 15).cgPath
@@ -33,11 +23,6 @@ class RankingTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        contentView.autoresizingMask = .flexibleHeight
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-        self.collectionView.isHidden = true
-
         self.mainImageView.layer.cornerRadius = self.mainImageView.frame.height / 2
     }
 
@@ -47,31 +32,11 @@ class RankingTableViewCell: UITableViewCell {
         self.nameLabel.text = rankingAttendance.person.name
         self.attendanceLabel.text = String(rankingAttendance.attendanceNumber)
         self.admonishmentLabel.text = String(rankingAttendance.admonishmentNumber)
-        DispatchQueue.main.async {
-            let imageString = CommonUtility.getProfileImageString(rankingAttendance.person)
-            self.mainImageView.image = UIImage(named: imageString)
-        }
-        if rankingType != .weekly { return }
-        self.morningDaysNumbers = self.createNumbersArray(rankingAttendance.morningDate)
-        self.eveningDaysNumbers = self.createNumbersArray(rankingAttendance.eveningDate)
-        DispatchQueue.main.async {
-            self.collectionView.reloadData()
-        }
+        let imageString = CommonUtility.getProfileImageString(rankingAttendance.person)
+        self.mainImageView.image = UIImage(named: imageString)
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        if self.isDetailViewHidden, selected {
-            self.collectionView.isHidden = false
-        } else {
-            self.collectionView.isHidden = true
-        }
-        contentView.layoutIfNeeded()
-        contentView.updateConstraints()
-        contentView.layoutIfNeeded()
-        setNeedsLayout()
-    }
-
+    
     func setUpShadow() {
         let cornerRadius: CGFloat = 15
         self.containerView.cornerRadius = cornerRadius
@@ -101,26 +66,5 @@ class RankingTableViewCell: UITableViewCell {
             break
         }
     }
-    
-    func handleShadowOnFriday(_ weekNumber: Int? = 0) {
-        // Show red and green cell only on friday and only if the week on focus is the current week
-        if Date().getDayNumberOfWeek() == 6, weekNumber == Date.now.getWeekNumber() {
-            // Check if the user has at least two presence or more than 2 admonishment
-            let attendance: Int = Int(attendanceLabel.text ?? "") ?? 0
-            let admonishment: Int = Int(admonishmentLabel.text ?? "") ?? 0
-            self.containerView.layer.shadowOpacity = 0.3
-            self.containerView.layer.shadowColor = attendance < 2 || admonishment >= 2
-            ? UIColor.red.cgColor
-            : Theme.customGreen.cgColor
-        }
-    }
-    
-    // Given an array of date create an array of Int that represents the specifc number of the day in the weel
-    func createNumbersArray(_ dates: [Date]) -> [Int] {
-        var datesNumbers: [Int] = []
-        for date in dates {
-            datesNumbers.append(date.getDayNumberOfWeek() ?? 1)
-        }
-        return datesNumbers
-    }
 }
+
