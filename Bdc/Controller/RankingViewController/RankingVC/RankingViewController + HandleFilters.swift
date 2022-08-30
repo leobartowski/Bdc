e5b15d394871c628203c8321d6b9973cf1588655
leobartowski <francescodangelo95@gmail.com>
2022-08-30 17:05:52 +0200

    DONE: Completed filter by slot (morning, evening, ...)

--- Bdc/Controller/RankingViewController/RankingVC/RankingViewController + HandleFilters.swift
+++ Bdc/Controller/RankingViewController/RankingVC/RankingViewController + HandleFilters.swift
@@ -32,7 +32,6 @@ extension RankingViewController {
                     cell.rankingType = vc.selectedType
                     self.rankingType = vc.selectedType
                     cell.handleChangeRankingType(oldRankingType)
-                    
                 }
                 return true
             }
@@ -40,4 +39,32 @@ extension RankingViewController {
             self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
         }
     }
+    
+    func presentModalToChangeSlotType() {
+        
+        let storyBoard = UIStoryboard(name: "BottomSheet", bundle: nil)
+        if let vc = storyBoard.instantiateViewController(withIdentifier: "slotTypeID") as? SlotTypeViewController {
+            // Set starting slot type (when first opening is always morning+evening)
+            vc.selectedSlot = self.slotType
+            let sheetController = SheetViewController(
+                controller: vc,
+                sizes: [.fixed(310)],
+                options: SheetOptions(shrinkPresentingViewController: false)
+            )
+            sheetController.gripSize = CGSize(width: 35, height: 6)
+            
+            sheetController.shouldDismiss = { _ in
+                // Update slotType based on user's choice
+                self.slotType = vc.selectedSlot
+                if let cell = self.tableView.cellForRow(at: IndexPath(row: 1, section: 0)) as? RankingSlotTypeTableViewCell {
+                    cell.setup(self.slotType)
+                    self.populateAttendance()
+                }
+                return true
+            }
+            self.present(sheetController, animated: true, completion: nil)
+            self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+        }
+    }
+
 }
