2ddd022eb375c5810d2113e6223c5c260bca0b2a
leobartowski <francescodangelo95@gmail.com>
2024-10-23 17:47:22 +0200

    DONE: Started setup for statistics

---
+++ Bdc/Controller/Statistics/StatisticsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  Bdc
+//
+//  Created by leobartowski on 23/10/24.
+//
+import UIKit
+
+class StatisticsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+}
