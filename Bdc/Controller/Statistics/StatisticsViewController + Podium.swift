a03e1f87e6ef214391280344dd630be9431a256c
leobartowski <francescodangelo95@gmail.com>
2024-12-03 19:17:04 +0100

    FIX

--- Bdc/Controller/Statistics/StatisticsViewController + Podium.swift
+++ Bdc/Controller/Statistics/StatisticsViewController + Podium.swift
@@ -24,12 +24,15 @@ extension StatisticsViewController {
     }
     
     fileprivate func createLabelsPodium() {
-        let percentage0 = Double(self.statsData.bestPals[0].value) / Double(self.statsData.totalAttendance) * 100
-        let percentage1 = Double(self.statsData.bestPals[1].value) / Double(self.statsData.totalAttendance) * 100
-        let percentage2 = Double(self.statsData.bestPals[2].value) / Double(self.statsData.totalAttendance) * 100
-        self.podiumFirstLabel.text = "\(self.statsData.bestPals[0].value) - \(String(format: "%.0f", percentage0))%"
-        self.podiumSecondLabel.text = "\(self.statsData.bestPals[1].value) - \(String(format: "%.0f", percentage1))%"
-        self.podiumThirdLabel.text = "\(self.statsData.bestPals[2].value) - \(String(format: "%.0f", percentage2))%"
+        if self.statsData.totalAttendance != 0 {
+            let percentage0 = Double(self.statsData.bestPals[0].value) / Double(self.statsData.totalAttendance) * 100
+            let percentage1 = Double(self.statsData.bestPals[1].value) / Double(self.statsData.totalAttendance) * 100
+            let percentage2 = Double(self.statsData.bestPals[2].value) / Double(self.statsData.totalAttendance) * 100
+            self.podiumFirstLabel.text = "\(self.statsData.bestPals[0].value) - \(String(format: "%.0f", percentage0))%"
+            self.podiumSecondLabel.text = "\(self.statsData.bestPals[1].value) - \(String(format: "%.0f", percentage1))%"
+            self.podiumThirdLabel.text = "\(self.statsData.bestPals[2].value) - \(String(format: "%.0f", percentage2))%"
+        }
+
     }
     
 }
